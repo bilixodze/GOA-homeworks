89982f048bcdf5a173a898c7e994ccdbec83e2ad
bilixodze <bilixodzeonise@gmail.com>
2024-02-19 22:34:14 +0400

    second commit

---
+++ DAY 04/les4 .py
@@ -0,0 +1,8 @@
+mother_age = 46
+father_age = 46
+sister_age = 27
+
+print(mother_age , father_age , sister_age )
+
+print("Now let's see their ages after 2 years")
+print(mother_age + 2, father_age + 2, sister_age + 2)
