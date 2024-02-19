3c7fb423198004cecd0fe9fc260142a335809d4e
bilixodze <bilixodzeonise@gmail.com>
2024-02-19 22:41:05 +0400

    second commit

---
+++ DAY 03/lesson3 .py
@@ -0,0 +1,6 @@
+mother = "xatuna_qoridze"   #დედა
+father = "paata_bilikhodze"     #მამა
+sister = "salome_bilikhodze"  #და 
+
+print("It's my mother's name", mother, ",It's my father's name", father,
+      ",It's my sister's name", sister )
