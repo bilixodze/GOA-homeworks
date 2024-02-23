e5d9fb2d91692eb18971e871111c3e7c8eaacaf2
bilixodze <bilixodzeonise@gmail.com>
2024-02-23 11:22:19 +0400

    second commit

--- DAY 04/homework/les4 .py
+++ DAY 04/homework/les4 .py
@@ -1,11 +1,9 @@
 
-# mother_age = 46
-# father_age = 46
-# sister_age = 27
+mother_age = 46
+father_age = 46
+sister_age = 27
 
-# print(mother_age , father_age , sister_age )
+print(mother_age , father_age , sister_age )
 
-# print("Now let's see their ages after 2 years")
-# print(mother_age + 2, father_age + 2, sister_age + 2)
-
-print(120 * 120)
+print("Now let's see their ages after 2 years")
+print(mother_age + 2, father_age + 2, sister_age + 2)
