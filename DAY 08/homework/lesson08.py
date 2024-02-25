c9bb42556ebd629c9b423c1f0c36eef4c39338cc
bilixodze <bilixodzeonise@gmail.com>
2024-02-25 16:33:53 +0400

    second commit

---
+++ DAY 08/homework/lesson08.py
@@ -0,0 +1,30 @@
+
+# გამოვიტანეთ რიცხვები 0_იდან 100_ის ჩათვლით
+
+for i in range(101) :
+   print(i)
+ 
+print("end first task ")
+print(" ")
+print("start second task")
+
+# შევკრიბეთ რიცხვები ათამდე
+
+for i in range (1) :
+  print(1 + 9)
+  print(2 + 8)
+  print(3 + 7)
+  print(4 + 6)
+  print(5 + 5)
+  print(6 + 4)
+  print(7 + 3)
+  print(8 + 2)
+  print(9 + 1)
+ 
+# ეს ბონუსი goa_სთვის <3 <3 <3
+
+print(" ")
+print("bonus")
+
+for i in range(20):
+   print("Goal-oriented academy")
