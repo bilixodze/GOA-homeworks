d741935002953092e2c35d14bf0fa126387362fc
bilixodze <bilixodzeonise@gmail.com>
2024-02-29 22:54:53 +0400

    second commit

---
+++ DAY 11/homework/lesson11.py
@@ -0,0 +1,94 @@
+
+# first task start
+
+"""Write a program that takes an input from the user and checks 
+if it's a positive, negative, or zero number using if-else"""
+
+# num1 = 25
+# num2 = 18
+
+# if int(input()) >= num1 :
+#     print("VIP server")
+# elif int(input()) >= num2 :
+#     print("standard server")
+# else:
+#     print("disconnect")
+
+# first task end
+
+
+# second task start
+    
+"""Write a program that prints all the even numbers between 
+1 and 20 using a for loop and if statement"""
+
+# even_numbers = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+# for i in even_numbers :
+#   print(i)
+
+# second task end
+
+
+# third task start
+
+"""Write a program that calculates the sum of a number entered
+ by the user using a while loop."""
+
+i = int(input())
+while i < 101:
+  print(i)
+  if i == 0:
+    break
+  i += 1
+
+# third task end 
+  
+
+# fourth task start
+  
+"""Write a program that simulates a basic ATM. It should ask the user for their PIN,
+ and if it's correct, display a text withdrawal, deposit, and balance"""
+
+# PIN = 0000
+
+# for i in range(4):
+#     input_PIN = int(input("Please enter your four-digital PIN :"))
+#     if input_PIN == PIN :
+#         print("Your PIN correct")
+#         print("Join basic ATM")
+#         print("______________")
+#         print("Your balance:")
+#         print("Deposit:")
+#         print("Exit:")
+#     else:
+#         print("Your PIN incorect, sorry your card blocked ")
+    
+#  fourth task end
+
+
+# fifth task start
+
+"""Write a program that simulates a simple login system. Ask the user for a username and password, 
+and if they enter "admin" and "password123", respectively, print "Login successful" using if-else."""
+
+
+# name = "admin"
+# password = "password123"
+
+# while True:
+#   input_name = input("please enter your username:")
+#   if input_name == name:
+#     print("Your username is correct.")
+#     break
+# else:
+#   print("your username incorect, please try again:")
+
+# while True:
+#   input_password = input("please enter your password:")
+#   if input_password == password:
+#     print("Your password is correct. You have successfully logged in")
+#     break
+# else:
+#   print("your pasword incorect, please try again:")
+
+  # fifth task end
