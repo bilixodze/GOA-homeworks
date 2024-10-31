eee4b38e6dc115213f112289e49a83de5c7c3f39
bilixodze <bilixodzeonise@gmail.com>
2024-10-31 21:03:40 +0400

    second commit

---
+++ DAY 85/classwork/script.js
@@ -0,0 +1,62 @@
+// ყველა ახალ ნასწავლი მეთოდის გამოყენებით შექმენით თითოეულ მათგანზე 2-2 მაგალითი და ახსენით კომენტარებით თქვენი სიტყვებით თუ რას აკეთებს 
+// თითოეული მეთოდი (push,pop,unshift,shift, slice, splice)
+
+// _____________________________________________________________
+
+// 1)
+
+// const name = ["onise", "luka", "nika"];
+
+// name.push("sandro") // ამატებს სიის ბოლოში
+
+// console.log(name)
+
+
+
+// 2)
+
+// const name = ["onise", "luka", "nika"];
+
+// name.pop() // შლის სიის ბოლოდან
+
+// console.log(name)
+
+
+
+// 3)
+
+// const name = ["onise", "luka", "nika"];
+
+// name.unshift("sandro") // ამატებს სიის თავში
+
+// console.log(name)
+
+
+
+// 4)
+
+// const name = ["onise", "luka", "nika"];
+
+// name.shift() // აშორებს სიის დასაწყისში
+
+// console.log(name)
+
+
+
+// 5)
+
+// const name = ["onise", "luka", "nika"];
+
+// console.log(name.slice(0,2))
+
+// console.log(name)
+
+
+
+// 6)
+
+// const name = ["onise", "luka", "nika"];
+
+// name.splice(0,1)
+
+// console.log(name)
